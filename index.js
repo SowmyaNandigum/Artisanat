bac587452976df801c7905da7737f5c92a535d58
SaiAbhinavGoud <90829704+SaiAbhinavGoud@users.noreply.github.com>
2022-10-16 10:46:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "React";
+import ReactDOM from "react-dom";
+import "./tryingcircles.css";
+import App from "./trying";
+
+ReactDOM.render(<App/>, document.getElementById("root"));
